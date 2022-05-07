961b96b43ce6fd792cd606c8702710d0362b7e5f
Curitis L <79395168+Curitis-L@users.noreply.github.com>
2022-05-07 21:40:18 +0800

    Update removecp.m

--- lab2/removecp.m
+++ lab2/removecp.m
@@ -5,11 +5,7 @@
 % Description :
 %   This file removes the cyclic prefix for OFDM symbols
 % -------------------------------------------------------------------------
-% Revisions   :
-%   Date                 Author            Description
-%   24-Apr-2022    Jiaxin Lyu        file adapted from large sim-environment
-% -------------------------------------------------------------------------
-%   Author: Jiaxin Lyu (e-mail: ljx981120@sjtu.edu.cn)
+% 
 % =========================================================================
 
 function data_out = removecp(data_in, len_of_gi)
